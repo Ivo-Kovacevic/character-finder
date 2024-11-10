b6cac1bbd4142246af665ba8e83c9d45ae3579ef
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-11-10 12:39:26 +0100

    make image responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { useEffect } from "react";
 import videoGameImage from "./assets/images/video-game-legends.jpg";
 
 export default function App() {
-  useEffect(() => {
-    document.body.style.overflowY = "hidden";
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <>
-      <img className="h-screen max-w-none" src={videoGameImage} alt="video-game-legends" />
+      <img className="video-game-legends" src={videoGameImage} alt="video-game-legends" />
     </>
   );
 }
