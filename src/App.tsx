401685d808ed244c42e8271a03009e477439fc3e
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-11-10 13:20:49 +0100

    get clicked image position

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { useEffect } from "react";
-import videoGameImage from "./assets/images/video-game-legends.jpg";
+import React, { MouseEvent, useEffect, useState } from "react";
+import videoGameImage from "../public/images/video-game-legends.jpg";
 
 export default function App() {
-  useEffect(() => {}, []);
+  const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
+
+  const handleImageClick = (e: React.MouseEvent<HTMLImageElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setClickPosition({ x, y });
+  };
+
+  useEffect(() => {
+    console.log(clickPosition);
+  }, [clickPosition]);
 
   return (
     <>
-      <img className="video-game-legends" src={videoGameImage} alt="video-game-legends" />
+      <img
+        className="video-game-legends"
+        onClick={(e) => handleImageClick(e)}
+        src={videoGameImage}
+        alt="video-game-legends"
+      />
     </>
   );
 }
