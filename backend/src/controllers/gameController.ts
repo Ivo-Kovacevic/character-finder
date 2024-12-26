ebaa9268ee0cc8dee59cc434e05317559b2c70b7
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-12-26 16:19:22 +0100

    add messages to responses for clarity

--- backend/src/controllers/gameController.ts
+++ backend/src/controllers/gameController.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from "express";
-import { v4 as uuid } from "uuid";
 import { StartBody, EndBody, ClickPositionType, CheckBody, CharacterType } from "../@types/express.js";
 import { clearSessionMiddleware } from "../middlewares/clearSessionMiddleware.js";
 import { checkPositionForCharacter, getRandomCharacters } from "../services/characterServices.js";
-import prisma from "../models/prisma.js";
 import { addRecordToLeaderboard, getLeaderboard } from "../services/gameServices.js";
 
-export const initGame = async (req: Request, res: Response) => {
+export const initGame = async (req: Request<{}, {}, {}, {}>, res: Response) => {
   const charactersToFind = await getRandomCharacters(3);
   const characterNames = charactersToFind.map((character) => character.name);
   req.session.charactersToFind = characterNames;
@@ -14,40 +12,41 @@ export const initGame = async (req: Request, res: Response) => {
   res.status(200).json(characterNames);
 };
 
-export const startGame = async (req: Request, res: Response) => {
+export const startGame = async (req: Request<{}, {}, {}, {}>, res: Response) => {
   req.session.startTime = Date.now();
 
-  res.sendStatus(200);
+  res.status(200).json({ message: "Game started" });
 };
 
 export const checkCharacterPosition = async (req: Request<{}, {}, CheckBody, {}>, res: Response) => {
   if (!req.session || !req.session.startTime || !req.session.charactersToFind) {
-    res.sendStatus(400);
+    res.status(400).json({ message: "No valid session was found" });
     return;
   }
 
   const characterName = req.body.characterName;
   const clickPosition = req.body.clickPosition;
-  
-  if (await checkPositionForCharacter(clickPosition, characterName)) {
-    req.session.charactersToFind = req.session.charactersToFind.filter((name) => name !== characterName);
-    
-    if (req.session.charactersToFind.length === 0) {
-      const endTime = Date.now();
-      const elapsedTime = Math.floor(endTime - req.session.startTime);
-      req.session.elapsedTime = elapsedTime;
 
-      res.status(200).json({ charactersToFind: req.session.charactersToFind, elapsedTime: elapsedTime });
-      return;
-    }
-
-    res.status(200).json({ charactersToFind: req.session.charactersToFind, elapsedTime: null });
-  } else {
-    res.sendStatus(400);
+  if (!(await checkPositionForCharacter(clickPosition, characterName))) {
+    res.status(400).json({ message: "Incorrect position for the character" });
+    return;
   }
+
+  req.session.charactersToFind = req.session.charactersToFind.filter((name) => name !== characterName);
+
+  if (req.session.charactersToFind.length === 0) {
+    const endTime = Date.now();
+    const elapsedTime = endTime - req.session.startTime;
+    req.session.elapsedTime = elapsedTime;
+
+    res.status(200).json({ charactersToFind: req.session.charactersToFind, elapsedTime: elapsedTime });
+    return;
+  }
+
+  res.status(200).json({ charactersToFind: req.session.charactersToFind, elapsedTime: null });
 };
 
-export const leaderboard = async (req: Request, res: Response) => {
+export const leaderboard = async (req: Request<{}, {}, {}, {}>, res: Response) => {
   const leaderboard = await getLeaderboard(10);
 
   res.status(200).json(leaderboard);
@@ -58,7 +57,7 @@ export const endGame = async (req: Request<{}, {}, EndBody, {}>, res: Response) 
   const { username } = req.body;
 
   if (!elapsedTime || !charactersToFind || charactersToFind.length > 0) {
-    res.sendStatus(400);
+    res.status(400).json({ message: "Could not save result to leaderboard" });
     return;
   }
 
